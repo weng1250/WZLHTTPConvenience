730693a5c4543280b2b9c7252c3c2fdf6996dd82
WengZilin <wzl@meitu.com>
2017-04-24 22:01:26 +0800

    【Feature】first commit

---
+++ WZLHTTPConvenience/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WZLHTTPConvenience
+//
+//  Created by WengZilin on 2017/4/24.
+//  Copyright © 2017年 com.Weng-Zilin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
